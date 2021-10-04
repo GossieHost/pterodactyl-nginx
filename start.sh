3a51cc78fbac9f945657f910e691b0d23c6b05e4
BasToTheMax <55401336+BasToTheMax@users.noreply.github.com>
2021-10-04 12:50:06 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,3 +5,35 @@ echo "Starting PHP-FPM..."
 
 echo "Starting Nginx..."
 /usr/sbin/nginx -c /home/container/nginx/nginx.conf
+
+
+docmd() {
+
+    read -p "user@WebHost:~ " CMD
+
+    eval "$CMD"
+
+    echo "user@WebHost:~ "
+
+    docmd2
+    
+
+}
+
+
+
+docmd2() {
+
+    read -p "user@WebHost:~ " CMD2
+
+    eval "$CMD2"
+
+    echo "user@WebHost:~ "
+
+    docmd
+    
+
+}
+
+echo "user@WebHost:~ "
+docmd
