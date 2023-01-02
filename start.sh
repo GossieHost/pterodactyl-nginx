dde5f7d1b6e931c1b81b2765fc86d9b1d9b7ef07
Bas <55401336+BasToTheMax@users.noreply.github.com>
2023-01-02 12:27:58 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,39 +1,7 @@
-#!/bin/ash
+#!/bin/bash
 
 echo "Starting PHP-FPM..."
 /usr/sbin/php-fpm7 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
 
 echo "Starting Nginx..."
 /usr/sbin/nginx -c /home/container/nginx/nginx.conf
-
-
-docmd() {
-
-    read -p "user@WebHost:~ " CMD
-
-    eval "$CMD"
-
-    echo "user@WebHost:~ "
-
-    docmd2
-    
-
-}
-
-
-
-docmd2() {
-
-    read -p "user@WebHost:~ " CMD2
-
-    eval "$CMD2"
-
-    echo "user@WebHost:~ "
-
-    docmd
-    
-
-}
-
-echo "user@WebHost:~ "
-docmd
